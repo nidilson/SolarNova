9a4ac882671ed285d474f16c934fc979db07ba21
Nidilson Rodríguez Carpio <nidilson.rodriguez@uned.cr>
2026-04-06 15:42:04 -0600

    Se modifica la página de inicio

---
+++ src/cookies.js
@@ -0,0 +1,6 @@
+const banner = document.getElementById("cookie-banner");
+const btn = document.getElementById("accept-cookies");
+
+btn.addEventListener("click", () => {
+banner.style.display = "none";
+});
